7c8060fec5fec9136cdf5604a974c91fbaec1a8b
Grant James Fairbrother <gjfairbrother@gmail.com>
2022-03-17 20:23:40 +0200

    Dataframe with selected Dates complete

---
+++ household.R
@@ -0,0 +1,10 @@
+library(dplyr)
+
+household_data <- read.table("household_power_consumption.txt", sep = ";", skip = 1, col.names = 
+                               c("Date","Time", "Global_active_power", "Global_reactive_power", "Voltage",
+                                 "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+household_data$Date <- strptime(household_data$Date, c("%d/%m/%Y")) %>%
+  as.Date(c("%d/%m/%Y"))
+
+household_data <- filter(household_data, Date == "2007-02-01" | Date == "2007-02-02")
