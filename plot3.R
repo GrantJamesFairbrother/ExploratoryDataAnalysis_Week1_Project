5b269567051e81cfcb7c9152f4891af133e2c108
Grant James Fairbrother <gjfairbrother@gmail.com>
2022-03-18 20:56:53 +0200

    Plot 3 done

--- plot3.R
+++ plot3.R
@@ -1,11 +1,15 @@
 source("household.R")
 
-png(file = "plot3.png")
+#png(file = "plot3.png")
 
 household_data$Time <- strptime(household_data$Time, format="%H:%M:%S")
 household_data[1:1440,"Time"] <- format(household_data[1:1440,"Time"],"2007-02-01 %H:%M:%S")
 household_data[1441:2880,"Time"] <- format(household_data[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
-plot(household_data$Time,c(as.numeric(household_data$Sub_metering_1), as.numeric(household_data$Sub_metering_2)),type="l",xlab="",ylab="Energy sub metering") 
+plot(household_data$Time, as.numeric(household_data$Sub_metering_1), type="l", xlab="", ylab="Energy sub metering")
+with(household_data, lines(Time, as.numeric(Sub_metering_2), col = "red"))
+with(household_data, lines(Time, as.numeric(Sub_metering_3), col = "blue"))
 
-dev.off()
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+#dev.off()
