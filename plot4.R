e09d8a467f779f0f6a28fb2c73b112281d415860
Grant James Fairbrother <gjfairbrother@gmail.com>
2022-03-18 21:05:50 +0200

    final

--- plot4.R
+++ plot4.R
@@ -8,7 +8,7 @@ household_data$Time <- strptime(household_data$Time, format="%H:%M:%S")
 household_data[1:1440,"Time"] <- format(household_data[1:1440,"Time"],"2007-02-01 %H:%M:%S")
 household_data[1441:2880,"Time"] <- format(household_data[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
-plot(household_data$Time,as.numeric(household_data$Global_active_power),type="l",xlab="",ylab="") 
+plot(household_data$Time,as.numeric(household_data$Global_active_power),type="l",xlab="",ylab="Global Active Power") 
 
 with(household_data, plot(Time, Voltage, type = "l", xlab = "datetime"))
 
