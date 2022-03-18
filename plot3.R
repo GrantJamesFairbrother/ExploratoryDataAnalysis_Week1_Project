f3a4aec41893bfc66f6ff7cf5496476dc671feca
Grant James Fairbrother <gjfairbrother@gmail.com>
2022-03-18 21:04:18 +0200

    Final plots complete

--- plot3.R
+++ plot3.R
@@ -1,6 +1,6 @@
 source("household.R")
 
-#png(file = "plot3.png")
+png(file = "plot3.png")
 
 household_data$Time <- strptime(household_data$Time, format="%H:%M:%S")
 household_data[1:1440,"Time"] <- format(household_data[1:1440,"Time"],"2007-02-01 %H:%M:%S")
@@ -12,4 +12,4 @@ with(household_data, lines(Time, as.numeric(Sub_metering_3), col = "blue"))
 
 legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-#dev.off()
+dev.off()
--- plot4.R
+++ plot4.R
@@ -1,7 +1,24 @@
 source("household.R")
 
-png(file = "plot2.png")
+png(file = "plot4.png")
 
+par(mfrow = c(2,2))
+
+household_data$Time <- strptime(household_data$Time, format="%H:%M:%S")
+household_data[1:1440,"Time"] <- format(household_data[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+household_data[1441:2880,"Time"] <- format(household_data[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+plot(household_data$Time,as.numeric(household_data$Global_active_power),type="l",xlab="",ylab="") 
+
+with(household_data, plot(Time, Voltage, type = "l", xlab = "datetime"))
+
+plot(household_data$Time, as.numeric(household_data$Sub_metering_1), type="l", xlab="", ylab="Energy sub metering")
+with(household_data, lines(Time, as.numeric(Sub_metering_2), col = "red"))
+with(household_data, lines(Time, as.numeric(Sub_metering_3), col = "blue"))
+
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+with(household_data, plot(Time, Global_reactive_power, type = "l", xlab = "datetime"))
 
 
 dev.off()
